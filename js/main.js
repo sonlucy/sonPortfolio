c71364e450b7e2d5f3686bcafc3648f258c0d17f
Bogyoi <shson2280@gmail.com>
2024-02-29 19:02:22 +0900

    스크롤 다운 시 요소 올라오는 작업 필요 - 원하는 요소 각각에 클래스 정의 필요?

--- js/main.js
+++ js/main.js
@@ -57,3 +57,27 @@ $(window).scroll(function(){
   } 
 });
 */
+
+// //////////////////////////////////////////////////
+
+
+/* <!--스크롤 이미지 애니메이션-- > */
+
+
+/* $(document).ready(function () {
+  $(window).scroll(function () {
+
+    $('image').each(function (i) {
+
+      var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 3;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_element) {
+        $(this).animate({
+          'opacity': '1'
+        }, 700);
+      }
+
+    });
+  });
+}); */
